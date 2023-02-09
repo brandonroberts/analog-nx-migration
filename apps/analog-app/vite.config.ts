d491d89d09a578ae82122b57b7f3b9008a09e2be
Brandon Roberts <robertsbt@gmail.com>
2023-02-09 15:08:57 -0600

    refactor: switch Angular app to use Analog and Vite

---
+++ apps/analog-app/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  publicDir: 'src/assets',
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      vite: {
+        tsconfig: 'apps/analog-app/tsconfig.app.json'
+      }
+    })
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test.ts'],
+    include: ['**/*.spec.ts'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
